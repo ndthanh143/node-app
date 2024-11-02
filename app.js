a510d8289072dabb77e60a5434e7e0e00ec1727a
alex-go-nguyen <alex.nguyen.goldenowl@gmail.com>
2024-11-03 00:04:48 +0700

    third commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World 2!');
+    res.send('Hello, World 3!');
 });
 
 app.listen(PORT, () => {
